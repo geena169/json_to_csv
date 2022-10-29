2712322763bdc0314cdf572c298bc2952f3b38ce
geena169 <40070905+geena169@users.noreply.github.com>
2022-10-30 02:45:42 +0300

    Update str_replace.py

--- str_replace.py
+++ str_replace.py
@@ -1,19 +1,4 @@
-
-"""
-def str_del_spec_symbols (_str : str):
-    while (x := _str.find("\n")) != -1:
-        _str = _str.replace('\n','')
-        
-    while (x := _str.find("\r")) != -1:
-        _str = _str.replace('\r','')
-        
-    while (x := _str.find("\t")) != -1:
-        _str = _str.replace('\t','')
-            
-
-    return _str
-"""
-
+# Удаление спецсимволов из строки
 
 def str_del_spec_symbols (_str : str):
     replace_values = {"\n": "", "\t": "", "\r": ""}
